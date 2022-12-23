8fea1bde0a5451a1bc1579195f3c7cf6fac2cbdc
towkir-turtle <towkir.webdeveloper@gmail.com>
2022-12-23 20:55:47 +0600

    project completed

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const searchTemperature = () => {
+  const API_KEY = `17a82c9c4be383c99479b61d3f1068a6`;
+  const city = document.getElementById("city-name").value;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTemperature(data));
+};
+
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
+
+const displayTemperature = (temperature) => {
+  setInnerText("city", temperature.name);
+  setInnerText("temperature", temperature.main.temp);
+  setInnerText("condition", temperature.weather[0].main);
+  // set weather icon
+  const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById("weather-icon");
+  imgIcon.setAttribute("src", url);
+};
